8bbbd49482702734c161dc2aedca206b876d984e
CesarAMBazan <zerolcamb@outlook.com>
2022-09-29 05:33:21 -0500

    Dialogue System Punctuation

--- Assets/Scripts/DialogueSystem/TypewriterEffect.cs
+++ Assets/Scripts/DialogueSystem/TypewriterEffect.cs
@@ -1,11 +1,21 @@
 
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class TypewriterEffect : MonoBehaviour
 {
     [SerializeField] private float typewriterSpeed = 50f;
+    
+    private readonly Dictionary<HashSet<char>, float> punctuations = new()
+    {
+        {new HashSet<char>(){'.', '!', '?'}, 0.6f},
+        {
+            new HashSet<char>() { ',', ';', ':' }, 0.3f
+        }
+    };
+
     public Coroutine Run(string textToType, TMP_Text textLabel)
     {
         return StartCoroutine(TypeText(textToType,textLabel));
@@ -20,15 +30,44 @@ public class TypewriterEffect : MonoBehaviour
 
         while (charIndex < textToType.Length)
         {
+            int lastCharIndex = charIndex;
+            
             t += Time.deltaTime * typewriterSpeed;
             charIndex = Mathf.FloorToInt(t);
             charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
 
-            textLabel.text = textToType.Substring(0, charIndex);
+            for (int i = lastCharIndex; i < charIndex; i++)
+            {
+                bool isLast = i >= textToType.Length - 1;
+                
+                textLabel.text = textToType.Substring(0, i + 1);
+
+                if (IsPunctuation(textToType[i], out float waitTime) && !isLast &&
+                    !IsPunctuation(textToType[i + 1], out _))
+                {
+                    yield return new WaitForSeconds(waitTime);
+                }
+            }
+            
             yield return null;
             
         }
 
         textLabel.text = textToType;
+    }
+
+    private bool IsPunctuation(char character, out float waitTime)
+    {
+        foreach (KeyValuePair<HashSet<char>, float> punctuactionCategory in punctuations)
+        {
+            if (punctuactionCategory.Key.Contains(character))
+            {
+                waitTime = punctuactionCategory.Value;
+                return true;
+            }
+        }
+
+        waitTime = default;
+        return false;
     }
 }
